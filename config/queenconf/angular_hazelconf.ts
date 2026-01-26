265888d61c09c18fed91a80103c1e9fef308f6ea
root <root@v22017064730750934.ultrasrv.de>
2026-01-26 20:39:05 +0100

    Update queenconf files

---
+++ config/queenconf/angular_hazelconf.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class Hazelconf {
+    public config = new Config();
+
+ }
+
+
+class Menu
+{
+};
+
+class Config
+{
+    static readonly rowsPerPage = 7;
+    static readonly showBlackFridayBanner = true;
+    static readonly showExtraHeader = false;
+    static readonly menu = new Menu();
+};
+
+
